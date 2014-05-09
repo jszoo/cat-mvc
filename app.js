033ed88478c069fafa7311c658b431d3c741d3bb
Ronglin-Kooboo <ronglin@kooboo.com>
2014-05-09 14:08:51 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var webserver = require('./lib/webserver');
 
 var DEFAULT_PORT = 1337;
-var argv_port = process.env.VMC_APP_PORT;
+var argv_port = process.env.PORT;
 
 webserver.create(argv_port || DEFAULT_PORT);

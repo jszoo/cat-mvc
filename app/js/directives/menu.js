af62485c14a8596cf0483bfcc380c8b14f089816
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-18 15:55:50 +0800

    test menu block

--- app/js/directives/menu.js
+++ app/js/directives/menu.js
@@ -5,10 +5,22 @@
 */
 
 module.exports = ['utils', function(utils) {
-	return {
-		scope: { current: '@' },
-		templateUrl: utils.viewUrl('views/directives/menu.html'),
-		restrict: 'E',
-		replace: true
-	};
+    var menuController = function($scope) {
+        this.scope = $scope;
+    };
+    menuController.prototype = {
+        scope: null,
+        constructor: menuController,
+        showPage: function(page) {
+            alert(page);
+        }
+    };
+    // ret
+    return {
+        scope: { current: '@' },
+        controller: menuController,
+        templateUrl: utils.viewUrl('views/directives/menu.html'),
+        restrict: 'E',
+        replace: true
+    };
 }];

4e9830e99d7037c1aa721ea472fe98937a0928fd
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-19 16:09:07 +0800

    update

--- app/js/controllers/all.js
+++ app/js/controllers/all.js
@@ -4,7 +4,7 @@
 * create date: 2014.5.4
 */
 
-var name = module.exports.name = 'Bloga.controllers';
+var name = module.exports.name = 'rulee.controllers';
 
 angular.module(name, [])
 .controller('MainCtrl', require('./main-controller'))

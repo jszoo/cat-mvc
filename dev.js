fe174606a5ec1c4ada4077032cbe303ab3b8320d
ruleechen <ronglin@kooboo.com>
2014-06-22 11:57:24 +0800

    update

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
-var webserver = require('./cms/node/webServer');
+var webServer = require('./cms/node/webServer');
 
 var DEFAULT_PORT = 8000;
 var argv_port = process.argv[2];
 
-webserver.create(argv_port || DEFAULT_PORT);
+webServer.create(argv_port || DEFAULT_PORT);

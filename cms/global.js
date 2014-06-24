7d5dad62f58e631d3fe6ffd5ce1bb968de59b268
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-24 11:09:53 +0800

    update

--- cms/global.js
+++ cms/global.js
@@ -9,7 +9,10 @@ var express = require('express');
 var app = express();
 
 var path = require('path');
-var abs = function(rel) { return path.join(__dirname, rel); };
+var abs = function(rel) { 
+	rel = rel.replace(/\//g, '\\');
+	return path.join(__dirname, rel);
+};
 
 // web config
 var setting = require('./node/setting');
@@ -27,7 +30,7 @@ app.use(logger('dev'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/hal+json' })); // parse application/hal+json as json
-app.use(bodyParser.urlencoded()); // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 // cookie
 var cookieParser = require('cookie-parser');
@@ -35,11 +38,11 @@ app.use(cookieParser());
 
 // dir mapping
 var favicon = require('serve-favicon');
-app.use(favicon(abs('fav.ico')));
+app.use(favicon(abs(config.get('favicon'))));
 app.use(express.static(abs('fe')));
 
 // load routes
-var routes = require('./routes');
+var routes = require('.' + config.get('routeTable'));
 utils.each(routes.pages(app), function() { app.use(this); });
 utils.each(routes.errors(app), function() { app.use(this); });
 

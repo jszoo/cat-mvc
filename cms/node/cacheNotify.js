7115073edbb8a3c013371ec090bf6ee048b275fb
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-25 13:42:31 +0800

    add verify

--- cms/node/cacheNotify.js
+++ cms/node/cacheNotify.js
@@ -12,10 +12,11 @@ var request = require('request'),
     config = require('./setting').load('/web.config');
 
 
+var verify = config.get('cacheNotify.verify');
 var servers = config.get('cacheNotify.servers');
 
 var combineUrl = function (url, params) {
-    params = utils.extend({}, params, { site: '' });
+    params = utils.extend({}, params, { verify: verify });
     return utils.setQuery(url, params);
 };
 
@@ -36,11 +37,13 @@ cache.storage.subscribe(function(params) {
 module.exports = {
     notify: function (params) {
         params = params || {};
-        if (params.action === 'remove' || params.action === 'clear') {
-            if (params.region && params.key) {
-                cache.storage.remove(params.region, params.key);
-            } else if (params.region) {
-                cache.storage.clear(params.region);
+        if (params.verify === verify) {
+            if (params.action === 'remove' || params.action === 'clear') {
+                if (params.region && params.key) {
+                    cache.storage.remove(params.region, params.key);
+                } else if (params.region) {
+                    cache.storage.clear(params.region);
+                }
             }
         }
     }

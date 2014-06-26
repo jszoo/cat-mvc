1fcef3b9a6785897d016b355851c91f98d183438
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-26 16:50:38 +0800

    rename controllers to ctrls

--- cms/areas/account/ctrls/logon.js
+++ cms/areas/account/ctrls/logon.js
@@ -12,19 +12,19 @@ module.exports = mvc.controller(function(req, res) {
     this.on('actionExecuted', function() { });
 
 
-    this.action('index', 'get', function() {
+    this.action('index', function() {
     });
 
     this.action('index', 'post', function() {
     });
 
-    this.action('forgotPassword', 'get', function() {
+    this.action('forgotPassword', function() {
     });
 
     this.action('forgotPassword', 'post', function() {
     });
 
-    this.action('resetPassword', 'get', function() {
+    this.action('resetPassword', function() {
     });
 
     this.action('resetPassword', 'post', function() {

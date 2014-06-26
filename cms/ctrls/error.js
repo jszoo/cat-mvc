9acc9314f36e648963e20bb9cf09d8b6c2b950e6
ruleechen <ronglin@kooboo.com>
2014-06-26 21:55:12 +0800

    add

---
+++ cms/ctrls/error.js
@@ -0,0 +1,14 @@
+/*
+* error
+* author: ronglin
+* create date: 2014.6.26
+*/
+
+var mvc = require('../node/mvc');
+
+module.exports = mvc.controller(function(req, res) {
+
+    this.action('index', function() {
+    });
+
+});

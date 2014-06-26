da0a9dfd75a2c237d1def0d12a258d99e6e839e0
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-26 16:32:52 +0800

    update

--- cms/areas/account/controllers/logon.js
+++ cms/areas/account/controllers/logon.js
@@ -5,24 +5,29 @@
 */
 
 var mvc = require('../../../node/mvc');
-var utils = require('../../../node/utilities');
 
 module.exports = mvc.controller(function(req, res) {
 
-    this.on('actionExecuting', function() {
+    this.on('actionExecuting', function() { });
+    this.on('actionExecuted', function() { });
 
-    });
-
-    this.on('actionExecuted', function() {
-
-    });
 
     this.action('index', 'get', function() {
-
     });
 
     this.action('index', 'post', function() {
+    });
 
+    this.action('forgotPassword', 'get', function() {
+    });
+
+    this.action('forgotPassword', 'post', function() {
+    });
+
+    this.action('resetPassword', 'get', function() {
+    });
+
+    this.action('resetPassword', 'post', function() {
     });
 
 });

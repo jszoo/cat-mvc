9f96e49051991ea9daf8edc662617df68d7c7907
ruleechen <ronglin@kooboo.com>
2014-07-01 01:22:32 +0800

    update

--- cms/node/mvcActionResults.js
+++ cms/node/mvcActionResults.js
@@ -6,78 +6,129 @@
 
 'use strict';
 
+var utils = require('./utilities');
+
+
 /* baseResult
 ***************************************/
-var baseResult = exports.baseResult = function() {
-	// body...
+var baseResult = exports.baseResult = function(set) {
+    utils.extend(this, set);
+};
+
+baseResult.prototype = {
+    constructor: baseResult,
+    execute: function(req, res) {
+    }
 };
 
 
 /* emptyResult
 ***************************************/
-var emptyResult = exports.emptyResult = function() {
-	// body...
+var emptyResult = exports.emptyResult = function(set) {
+    utils.extend(this, set);
+};
+
+emptyResult.prototype = {
+    constructor: emptyResult
 };
 
 
 /* httpNotFoundResult
 ***************************************/
-var httpNotFoundResult = exports.httpNotFoundResult = function() {
-	// body...
+var httpNotFoundResult = exports.httpNotFoundResult = function(set) {
+    utils.extend(this, set);
+};
+
+httpNotFoundResult.prototype = {
+    constructor: httpNotFoundResult
 };
 
 
 /* fileResult
 ***************************************/
-var fileResult = exports.fileResult = function() {
-	// body...
+var fileResult = exports.fileResult = function(set) {
+    utils.extend(this, set);
+};
+
+fileResult.prototype = {
+    constructor: fileResult
 };
 
 
 /* jsonResult
 ***************************************/
-var jsonResult = exports.jsonResult = function() {
-	// body...
+var jsonResult = exports.jsonResult = function(set) {
+    utils.extend(this, set);
+};
+
+jsonResult.prototype = {
+    constructor: jsonResult
 };
 
 
 /* partialViewResult
 ***************************************/
-var partialViewResult = exports.partialViewResult = function() {
-	// body...
+var partialViewResult = exports.partialViewResult = function(set) {
+    utils.extend(this, set);
+};
+
+partialViewResult.prototype = {
+    constructor: partialViewResult
 };
 
 
 /* viewResult
 ***************************************/
-var viewResult = exports.viewResult = function() {
-	// body...
+var viewResult = exports.viewResult = function(set) {
+    utils.extend(this, set);
+};
+
+viewResult.prototype = {
+    constructor: viewResult
 };
 
 
 /* contentResult
 ***************************************/
-var contentResult = exports.contentResult = function() {
-	// body...
+var contentResult = exports.contentResult = function(set) {
+    utils.extend(this, set);
+};
+
+contentResult.prototype = {
+    constructor: contentResult
 };
 
 
 /* redirectResult
 ***************************************/
-var redirectResult = exports.redirectResult = function() {
-	// body...
+var redirectResult = exports.redirectResult = function(set) {
+    utils.extend(this, set);
+};
+
+redirectResult.prototype = {
+    constructor: redirectResult
 };
 
 
 /* redirectToActionResult
 ***************************************/
-var redirectToActionResult = exports.redirectToActionResult = function() {
-	// body...
+var redirectToActionResult = exports.redirectToActionResult = function(set) {
+    utils.extend(this, set);
+};
+
+redirectToActionResult.prototype = {
+    constructor: redirectToActionResult
 };
 
 
 /* redirectToActionPermanentResult
 ***************************************/
-var redirectToActionPermanentResult = exports.redirectToActionPermanentResult = function() {
-	// body...
+var redirectToActionPermanentResult = exports.redirectToActionPermanentResult = function(set) {
+    utils.extend(this, set);
 };
+
+redirectToActionPermanentResult.prototype = {
+    constructor: redirectToActionPermanentResult
+};
+
+

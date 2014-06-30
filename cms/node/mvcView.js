24197432b4d52a1e0fb825c0b2569001483c430f
Ronglin-Kooboo <ronglin@kooboo.com>
2014-06-30 14:36:07 +0800

    update

---
+++ cms/node/mvcView.js
@@ -0,0 +1,28 @@
+/*
+* mvcView
+* author: ronglin
+* create date: 2014.6.26
+*/
+
+'use strict';
+
+var utils = require('./utilities');
+
+var mvcView = function(name, options) {
+    debugger;
+};
+
+mvcView.prototype = {
+    
+    constructor: mvcView,
+
+    lookup: function(path) {
+        debugger;
+    },
+
+    render:function(options, fn) {
+        debugger;
+    }
+};
+
+module.exports = mvcView;

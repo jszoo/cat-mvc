2ab6c51497c094d39cab56256662021025b9a25c
Ronglin-Kooboo <ronglin@kooboo.com>
2014-07-01 10:32:35 +0800

    more readable match handler

--- cms/node/mvc.js
+++ cms/node/mvc.js
@@ -25,31 +25,31 @@ var mvcHandler = function(set) {
     //
     return function(req, res, next) {
         var pathname = parse(req.url).pathname;
-        var allAreas = mvcAreas.all(), matched = false;
+        var matched = false, allAreas = mvcAreas.all();
         utils.each(allAreas, function(i, area) {
             if (matched) { return false; }
             utils.each(area.routes, function(k, route) {
                 var match = macher(route.expression);
                 var params = match(pathname);
-                if (params !== false) {
-                    var ctrlParam = params[0];
-                    if (ctrlParam) {
-                        var ctrl = area.controllers[ctrlParam.value || route.defaultValues[ctrlParam.name]];
-                        if (ctrl) {
-                            var actParam = params[1];
-                            if (actParam) {
-                                ctrl.initialize(req, res);
-                                var actions = ctrl.actions();
-                                var act = actions[actParam.value || route.defaultValues[actParam.name]];
-                                if (act) {
-                                    act.execute(req, res);
-                                    matched = true;
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
+                if (params === false) { return; }
+                //
+                var ctrlParam = params[0];
+                if (!ctrlParam) { return; }
+                //
+                var ctrl = area.controllers[ctrlParam.value || route.defaultValues[ctrlParam.name]];
+                if (!ctrl) { return; }
+                //
+                var actParam = params[1];
+                if (!actParam) { return; }
+                //
+                ctrl.initialize(req, res);
+                var actions = ctrl.actions();
+                var act = actions[actParam.value || route.defaultValues[actParam.name]];
+                if (!act) { return; }
+                //
+                act.execute(req, res);
+                matched = true;
+                return false;
             });
         });
         if (!matched) {

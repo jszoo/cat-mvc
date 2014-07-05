06f6adcd2e1609fae6beac2950a3aacb54d91311
ruleechen <ronglin@kooboo.com>
2014-07-05 10:40:14 +0800

    update

--- cms/node/mvcArea.js
+++ cms/node/mvcArea.js
@@ -57,42 +57,71 @@ mvcArea.prototype = {
         return (this.routes = {}, this);
     },
 
-    loadController: function(filePath) {
-        if (fs.statSync(filePath).isFile()) {
-            var self = this, coreLoad = function(ctrl) {
-                if (ctrl && ctrl.className === 'mvcController') {
-                    if (!ctrl.name()) {
-                        var extName = path.extname(filePath);
-                        ctrl.name(path.basename(filePath, extName));
-                    }
-                    ctrl.path(filePath);
-                    self.controllers[ctrl.name().toLowerCase()] = ctrl;
-                    return true;
-                }
-            };
-            var module = require(filePath);
-            if (module && coreLoad(module) !== true) {
-                utils.each(module, function() {
-                    coreLoad(this);
-                });
-            }
+    registerController: function(controllerName, controller) {
+        if (arguments.length === 1) {
+            controller = controllerName;
+            controllerName = null;
         }
-        return this;
+        if (controller && controller.className === 'mvcController') {
+            controllerName = controllerName || controller.name();
+            this.controllers[controllerName.toLowerCase()] = controller;
+        }
     },
 
-    unloadController: function(filePath) {
-        var extName = path.extname(filePath);
-        var ctrlName = path.basename(filePath, extName);
-        return (delete this.controllers[ctrlName.toLowerCase()], this);
+    removeController: function(controllerName) {
+        controllerName = (controllerName || '');
+        return (delete this.controllers[controllerName.toLowerCase()]);
     },
 
-    findController: function(name) {
-        name = (name || '').toLowerCase();
-        return this.controllers[name];
+    findController: function(controllerName) {
+        controllerName = (controllerName || '');
+        return (this.controllers[controllerName.toLowerCase()]);
     },
 
     clearControllers: function() {
         return (this.controllers = {}, this);
+    },
+
+    loadController: function(filePath) {
+        if (fs.statSync(filePath).isFile()) { return; }
+        var self = this, load = function(ctrl) {
+            if (ctrl && ctrl.className === 'mvcController') {
+                if (!ctrl.name()) {
+                    var extName = path.extname(filePath);
+                    ctrl.name(path.basename(filePath, extName));
+                }
+                ctrl.path(filePath);
+                self.registerController(ctrl.name(), ctrl);
+                return true;
+            }
+        };
+        var module = require(filePath);
+        if (module && load(module) !== true) {
+            utils.each(module, function() {
+                load(this);
+            });
+        }
+    },
+
+    unloadController: function(filePath) {
+        if (fs.statSync(filePath).isFile()) { return; }
+        var self = this, unload = function(ctrl) {
+            if (ctrl && ctrl.className === 'mvcController') {
+                var ctrlName = ctrl.name();
+                if (!ctrlName) {
+                    var extName = path.extname(filePath);
+                    ctrlName = path.basename(filePath, extName);
+                }
+                self.removeController(ctrlName);
+                return true;
+            }
+        };
+        var module = require(filePath);
+        if (module && unload(module) !== true) {
+            utils.each(module, function() {
+                unload(this);
+            });
+        }
     },
 
     loadExtension: function(filePath) {

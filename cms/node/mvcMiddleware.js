59c86ab2f50fa8486a16bd67d9833cd4e03bac5f
ruleechen <ronglin@kooboo.com>
2014-07-06 00:27:52 +0800

    update

--- cms/node/mvcMiddleware.js
+++ cms/node/mvcMiddleware.js
@@ -10,52 +10,54 @@ var utils = require('./utilities');
 
 exports.xHeaders = function(set) {
     set = set || {};
-    var process = function(req, res) {
-        res.setHeader('X-Powered-By', 'RULEE');
-        utils.each(set.headers, function(key, value) {
-            res.setHeader(key, value);
-        });
+    return {
+        handle: function(req, res) {
+            res.setHeader('X-Powered-By', 'RULEE');
+            utils.each(set.headers, function(key, value) {
+                res.setHeader(key, value);
+            });
+        }
     };
-    // ret
-    return { handle: process };
 };
 
 exports.ruleeUrl = function(set) {
     set = set || {};
-    var process = function(req, res) {
-        //
-        var rulee = req.rulee || (req.rulee = {});
-        var url = rulee.url || (rulee.url = {});
-        //
-        var host = req.headers.host; //eg: www.nodetest.cn:1337
-        var path = req.url;          //eg: /home
+    return {
+        handle: function(req, res) {
+            var rulee = req.rulee || (req.rulee = {});
+            var url = rulee.url = {};
+            url.host = req.headers.host; //eg: www.nodetest.cn:1337
+            url.path = req.url;          //eg: /home
+        }
     };
-    // ret
-    return { handle: process };
 };
 
 exports.ruleeForm = function(set) {
     set = set || {};
-    var process = function(req, res) {
-        //
-        var rulee = req.rulee || (req.rulee = {});
-        var form = rulee.form || (rulee.form = {});
-        //
-        utils.extend(form, req.body);
+    return {
+        handle: function(req, res) {
+            var rulee = req.rulee || (req.rulee = {});
+            utils.extend(rulee.form = {}, req.body);
+        }
     };
-    // ret
-    return { handle: process };
 };
 
 exports.ruleeQuery = function(set) {
     set = set || {};
-    var process = function(req, res) {
-        //
-        var rulee = req.rulee || (req.rulee = {});
-        var query = rulee.query || (rulee.query = {});
-        //
-        utils.extend(query, req.query);
+    return {
+        handle: function(req, res) {
+            var rulee = req.rulee || (req.rulee = {});
+            utils.extend(rulee.query = {}, req.query);
+        }
     };
-    // ret
-    return { handle: process };
+};
+
+exports.ruleeSecure = function(set) {
+    set = set || {};
+    return {
+        handle: function(req, res) {
+            var rulee = req.rulee || (req.rulee = {});
+            rulee.secure = !!req.secure;
+        }
+    };
 };

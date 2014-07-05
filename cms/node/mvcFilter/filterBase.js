b79d7b2e995220cd666fea4525e20ea370c48e6d
ruleechen <ronglin@kooboo.com>
2014-07-05 16:08:28 +0800

    update

--- cms/node/mvcFilter/filterBase.js
+++ cms/node/mvcFilter/filterBase.js
@@ -7,11 +7,11 @@
 'use strict';
 
 var baseFilter = function(set) {
-	utils.extend(this, set);
+    utils.extend(this, set);
 };
 
 baseFilter.prototype = {
 
-	constructor: baseFilter, className: 'mvcFilter'
+    constructor: baseFilter, className: 'mvcFilter'
 
 };

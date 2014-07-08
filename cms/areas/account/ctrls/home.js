26b6d6cde53f4d48c6c10663918da7b44cd426ae
Ronglin-Kooboo <ronglin@kooboo.com>
2014-07-08 12:49:34 +0800

    update

--- cms/areas/account/ctrls/home.js
+++ cms/areas/account/ctrls/home.js
@@ -9,7 +9,7 @@ var mvc = require('../../../node/mvc');
 module.exports = mvc.controller(function(req, res, end) {
 
     this.action('index', function() {
-        return this.redirectToAction('test');
+        return this.redirectToAction('test','home', { id: 1 });
     });
 
     this.action('test', function() {

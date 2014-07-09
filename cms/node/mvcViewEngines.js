7c4f8e1aca5e9eae2fee4aa23ee3b762f9927d68
Ronglin-Kooboo <ronglin@kooboo.com>
2014-07-09 17:34:59 +0800

    update

--- cms/node/mvcViewEngines.js
+++ cms/node/mvcViewEngines.js
@@ -9,25 +9,25 @@
 var utils = require('./utilities'),
     caching = require('./caching');
 
-var inner = caching.region('mvc-view-engines-cache');
+var _inner = caching.region('mvc-view-engines-cache');
 
 module.exports = {
 
     register: function(extname, engine) {
         if (!extname) { throw new Error('Parameter "extname" is required'); }
         if (!utils.isFunction(engine)) { throw new Error('Parameter "engine" is required a function'); }
-        return inner.set(extname, engine);
+        return _inner.set(extname, engine);
     },
 
     remove: function(extname) {
-        return inner.remove(extname);
+        return _inner.remove(extname);
     },
 
     get: function(extname) {
-        return inner.get(extname);
+        return _inner.get(extname);
     },
 
     clear: function() {
-        return inner.clear();
+        return _inner.clear();
     }
 };

f797136d77e5d26a9c81db9845bac03faeefc098
Ronglin-Kooboo <ronglin@kooboo.com>
2014-07-11 11:06:39 +0800

    update

--- cms/node/mvcHandler.js
+++ cms/node/mvcHandler.js
@@ -45,81 +45,78 @@ module.exports = function(setts) {
             }
         };
         //
-        var allAreas = mvcAreas.all();
-        utils.each(allAreas, function(i, area) {
-            if (matched || exception) { return false; } // break
+        var routeSet = mvcAreas.routeSet();
+        utils.each(routeSet, function(n, route) {
+            var routeData = route.routeData(rulee.request.url.pathname);
+            if (!routeData) { return; } // continue
             //
-            utils.each(area.ownedRoutes(), function(k, route) {
-                var routeData = route.routeData(rulee.request.url.pathname);
-                if (!routeData) { return; } // continue
-                //
-                var areaParam = getParam(routeData, 'area');
-                if (!areaParam) {
-                    routeData.unshift({
-                        name: 'area',
-                        value: area.name
-                    });
-                }
-                //
-                var controllerParam = getParam(routeData, 'controller');
-                if (!controllerParam) { return; } // continue
-                //
-                var controller = area.findController(controllerParam.value);
-                if (!controller) { return; } // continue
-                //
-                var actionParam = getParam(routeData, 'action');
-                if (!actionParam) { return; } // continue
-                //
-                var httpContext = new mvcContext({
-                    request: req,
-                    response: res,
-                    route: route,
-                    routeData: routeData,
-                    routeArea: area,
-                    routeSet: mvcAreas.routeSet(),
-                    rulee: rulee
+            var area = mvcAreas.get(route.ownerAreaName);
+            var areaParam = getParam(routeData, 'area');
+            if (!areaParam) {
+                routeData.unshift({
+                    name: 'area',
+                    value: area.name
                 });
-                try {
-                    controller = controller.clone();
-                    controller.initialize(httpContext);
-                    controller.executeImpl();
-                } catch (ex) {
-                    controller.destroy();
-                    exception = ex;
-                    return false; // break
-                }
-                //
-                var action = controller.findAction(actionParam.value, rulee.request.method, rulee.request.secure);
-                if (!action) { 
-                    controller.destroy();
-                    return; // continue
-                }
-                //
-                try {
-                    matched = true;
-                    var actionExecuted = false;
-                    action.executeImpl(function(obj) {
-                        if (obj === undefined) { return; }
-                        if (actionExecuted) { return; }
-                        actionExecuted = true;
-                        //
-                        var resultExecuted = false;
-                        action.executeResult(obj, function(error) {
-                            if (resultExecuted) { return; }
-                            resultExecuted = true;
-                            controller.destroy();
-                            exception = error;
-                            gotoNext();
-                        });
-                    });
-                } catch (ex) {
-                    controller.destroy();
-                    exception = ex;
-                    return false; // break
-                }
-                //
-                return false; // break
+            }
+            //
+            var controllerParam = getParam(routeData, 'controller');
+            if (!controllerParam) { return; } // continue
+            //
+            var controller = area.findController(controllerParam.value);
+            if (!controller) { return; } // continue
+            //
+            var actionParam = getParam(routeData, 'action');
+            if (!actionParam) { return; } // continue
+            //
+            var httpContext = new mvcContext({
+                request: req,
+                response: res,
+                route: route,
+                routeData: routeData,
+                routeArea: area,
+                routeSet: routeSet,
+                rulee: rulee
             });
+            try {
+                controller = controller.clone();
+                controller.initialize(httpContext);
+                controller.executeImpl();
+            } catch (ex) {
+                controller.destroy();
+                exception = ex;
+                return false; // break
+            }
+            //
+            var action = controller.findAction(actionParam.value, rulee.request.method, rulee.request.secure);
+            if (!action) { 
+                controller.destroy();
+                return; // continue
+            }
+            //
+            try {
+                matched = true;
+                var actionExecuted = false;
+                action.executeImpl(function(obj) {
+                    if (obj === undefined) { return; }
+                    if (actionExecuted) { return; }
+                    actionExecuted = true;
+                    //
+                    var resultExecuted = false;
+                    action.executeResult(obj, function(error) {
+                        if (resultExecuted) { return; }
+                        resultExecuted = true;
+                        controller.destroy();
+                        exception = error;
+                        gotoNext();
+                    });
+                });
+            } catch (ex) {
+                controller.destroy();
+                exception = ex;
+                return false; // break
+            }
+            //
+            return false; // break
         });
         // next
         gotoNext();

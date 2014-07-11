fd04b63742546646ae3ba403749572311daec51b
Ronglin-Kooboo <ronglin@kooboo.com>
2014-07-11 15:42:07 +0800

    update

--- cms/ctrls/notfound.js
+++ cms/ctrls/notfound.js
@@ -9,8 +9,7 @@ var mvc = require('../node/mvc');
 module.exports = mvc.controller(function(req, res, end) {
 
     this.action('index', function() {
-        res.render('admin');
-        //end.view('admin');
+        end.view();
     });
 
 });

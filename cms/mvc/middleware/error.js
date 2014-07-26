832b7c4b50c118953feb052ae96aa7c60713b1c8
ruleechen <ronglin@kooboo.com>
2014-07-26 13:27:10 +0800

    update

--- cms/mvc/middleware/error.js
+++ cms/mvc/middleware/error.js
@@ -9,18 +9,26 @@
 module.exports = function() {
     //
     var format = function(err) {
-        var msg = [];
-        msg.push('Message: ' + err.message);
-        msg.push('Status: ' + err.status);
-        msg.push(err.stack);
-        return msg.join('\n\n');
+        var msg = ['<!DOCTYPE html>'];
+        msg.push('<html>');
+        msg.push('<head>');
+        msg.push('  <title>Internal Server Error</title>');
+        msg.push('</head>');
+        msg.push('<body>');
+        msg.push('  <h1>' + err.message + '</h1>');
+        msg.push('  <h3>Status: ' + err.status + '</h3>')
+        msg.push('  <pre>' + err.stack + '</pre>');
+        msg.push('</body>');
+        msg.push('</html>');
+        return msg.join('\n');
     };
     //
     return function(req, res, next, err) {
         if (err) {
             if (!(err instanceof Error)) { err = new Error(err); }
-            var ct = { 'Content-Type': 'text/plain' };
-            err.status = err.status || 500;
+            if (!err.message) { err.message = 'Internal Server Error'; }
+            if (!err.status) { err.status = 500; }
+            var ct = { 'Content-Type': 'text/html' };
             res.writeHead(err.status, ct);
             res.end(format(err));
         } else {

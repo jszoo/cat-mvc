bc87a41c5c729bd86e53bf3f903423d9d97c3deb
jszoo <jszoo@outlook.com>
2014-07-31 10:28:03 +0800

    update

--- mvc/attribute/dataType.js
+++ mvc/attribute/dataType.js
@@ -1,5 +1,5 @@
 /*
-* fieldDataType
+* dataType
 * author: ruleechen
 * contact: rulee@live.cn
 * create date: 2014.7.31
---
+++ mvc/attribute/validation.js
@@ -0,0 +1,62 @@
+/*
+* validation
+* author: ruleechen
+* contact: rulee@live.cn
+* create date: 2014.7.31
+*/
+
+'use strict';
+
+var types = [
+    'empty',
+    'required',
+    'notEmpty',
+    'undefined',
+    'string',
+    'alpha',
+    'numeric',
+    'alphanumeric',
+    'email',
+    'url',
+    'urlish',
+    'ip',
+    'ipv4',
+    'ipv6',
+    'creditcard',
+    'uuid',
+    'uuidv3',
+    'uuidv4',
+    'int',
+    'integer',
+    'number',
+    'finite',
+    'decimal',
+    'float',
+    'falsey',
+    'truthy',
+    'null',
+    'notNull',
+    'boolean',
+    'array',
+    'date',
+    'hexadecimal',
+    'hexColor',
+    'lowercase',
+    'uppercase',
+    'after',
+    'before',
+    'is',
+    'regex',
+    'not',
+    'notRegex',
+    'equals',
+    'contains',
+    'notContains',
+    'len',
+    'in',
+    'notIn',
+    'max',
+    'min',
+    'minLength',
+    'maxLength'
+];

67305f24a24e21eb25ca001448ad2da58ac1d5b2
jszoo <jszoo@outlook.com>
2014-07-31 10:21:54 +0800

    update

---
+++ mvc/attribute/fieldDataType.js
@@ -0,0 +1,21 @@
+/*
+* fieldDataType
+* author: ruleechen
+* contact: rulee@live.cn
+* create date: 2014.7.31
+*/
+
+'use strict';
+
+var types = [
+    'string',
+    'integer',
+    'float',
+    'date',
+    'time',
+    'datetime',
+    'boolean',
+    'binary',
+    'array',
+    'json'
+];

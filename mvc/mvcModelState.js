59ca8f6065f2052b1d75eef980e5e50e614c3f9c
jszoo <jszoo@outlook.com>
2014-08-08 14:57:53 +0800

    update

--- mvc/mvcModelState.js
+++ mvc/mvcModelState.js
@@ -11,9 +11,28 @@ var utils = require('zoo-utils');
 
 var mvcModelState = module.exports = function(set) {
     utils.extend(this, set);
+    this._inner = new utils.dictionary(true);
 };
 
 mvcModelState.prototype = {
 
-    constructor: mvcModelState, className: 'mvcModelState'
+    _inner: null,
+
+    constructor: mvcModelState,
+
+    addModelError: function(key, error) {
+
+    },
+
+    isValidField: function(key) {
+        
+    },
+
+    remove: function(key) {
+        this._inner.remove(key);
+    },
+
+    clear: function() {
+        this._inner.clear();
+    }
 };

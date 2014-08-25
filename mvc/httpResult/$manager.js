50a7b57501de698c7d5288562dc9d3502174b678
jszoo <jszoo@outlook.com>
2014-08-25 17:46:17 +0800

    update

--- mvc/httpResult/$manager.js
+++ mvc/httpResult/$manager.js
@@ -9,7 +9,6 @@
 
 var utils = require('zoo-utils'),
     caching = require('zoo-cache'),
-    mvcActionResult = require('../mvcActionResult'),
     mvcActionResultApi = require('../mvcActionResultApi');
 
 var actionResultManager = module.exports = function(store) {
@@ -35,7 +34,12 @@ actionResultManager.prototype = {
     },
 
     remove: function(name) {
-        return this._inner.remove(name);
+        if (actionResultApi.remove(name)) {
+            this._inner.remove(name);
+            return true;
+        } else {
+            return false;
+        }
     },
 
     count: function() {
@@ -43,6 +47,7 @@ actionResultManager.prototype = {
     },
 
     clear: function() {
+        actionResultApi.clear();
         this._inner.clear();
         return this;
     },
@@ -52,6 +57,7 @@ actionResultManager.prototype = {
         if (!name) { throw new Error('Action result name is required'); }
         if (this.exists(name)) { throw new Error(utils.format('Action result "{0}" already exists', name)); }
         if (!utils.isFunction(klass)) { throw new Error(utils.format('Action result "{0}" requires function type class but got {1} type', name, utils.type(klass))); }
+        actionResultApi.set(name, klass);
         this._inner.set(name, klass);
         return this;
     },
@@ -63,5 +69,36 @@ actionResultManager.prototype = {
     discover: function() {
         var self = this;
         utils.each(require('./actionResult'), function(name) { self.register(name, this); });
+    }
+};
+
+
+var proto = mvcActionResultApi.prototype;
+var nativeNames = 'httpContext,sync,callback,with,constructor,';
+
+var actionResultApi = {
+    allow: function(name) {
+        return (nativeNames.indexOf(name + ',') === -1);
+    },
+    set: function(name, func) {
+        if (!this.allow(name)) {
+            throw new Error(utils.format('Action result name "{0}" is not allow', name));
+        }
+        proto[name] = function() {
+            return this.with(func.apply(null, arguments));
+        };
+    },
+    remove: function(name) {
+        if (this.allow(name) && proto[name] !== undefined) {
+            proto[name] = undefined;
+            return true;
+        } else {
+            return false;
+        }
+    },
+    clear: function() {
+        utils.each(proto, function(name) {
+            actionResultApi.remove(name);
+        });
     }
 };
